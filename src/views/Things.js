da156f8ccf0057237e5a11955b0e0e9f59d85b8f
KIM Kangseong <KIM.Kangseong@gmail.com>
2018-01-29 11:41:36 +0900

    Update Things view

--- src/views/Things.js
+++ src/views/Things.js
@@ -2,22 +2,60 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {View, Text} from 'react-native';
 
+import Container from 'components/Container';
+
+const Thing = (props) => {
+  const {
+    thing,
+    dates,
+    style,
+    nameStyle,
+    datesStyle,
+    dateStyle
+  } = props;
+
+  return (
+    <View style={style}>
+      <Text style={nameStyle}>{thing}</Text>
+      <View style={datesStyle}>
+        {dates.map((d,j) =>
+          <Text key={j} style={dateStyle}>{d}</Text>
+        )}
+      </View>
+    </View>
+  );
+};
+
+Thing.defaultProps = {
+  style: {
+    width: '50%',
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'white',
+    backgroundColor: '#EFEFEF',
+  },
+  nameStyle: {
+    fontSize: 18
+  },
+  datesStyle: {
+    marginLeft: 10
+  },
+  dateStyle: {
+    color: '#666'
+  }
+};
+
 class ThingsView extends Component {
   render() {
     const {things} = this.props;
     return (
-      <View>
-        {Object.keys(things).map((k,i) => 
-          <View key={i} style={{marginBottom: 10}}>
-            <Text>{things[k].thing}</Text>
-            <View style={{marginLeft: 10}}>
-              {things[k].dates.map((d,j) =>
-                <Text key={j}>{d}</Text>
-              )}
-            </View>
-          </View>
-        )}
-      </View>
+      <Container>
+        <View style={{flexDirection: 'row', flexWrap: 'wrap', width: '100%'}}>
+          {Object.keys(things).map((k,i) =>
+            <Thing key={i} {...things[k]} />
+          )}
+        </View>
+      </Container>
     );
   }
 }
