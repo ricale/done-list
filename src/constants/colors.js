293744920ad35ad8c1332e62299eb58bc68f2d4e
KIM Kangseong <KIM.Kangseong@gmail.com>
2018-02-13 19:59:08 +0900

    Add more colors for things [Finishes #155073595]

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -4,9 +4,22 @@ export const colors = [
   "#ffffba",
   "#baffc9",
   "#bae1ff",
+
   "#258787",
   "#66c880",
   "#f3dd7e",
   "#eb872a",
-  "#db4f56"
+  "#db4f56",
+
+  '#e1f7d5',
+  '#c9c9ff',
+  '#ffffff',
+  '#f1cbff',
+  '#b88c8c',
+
+  '#ddadad',
+  '#d6c7c7',
+  '#aec8ce',
+  '#f3e8e8',
+  '#5c745b',
 ];
